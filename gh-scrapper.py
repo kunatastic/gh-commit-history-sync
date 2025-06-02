a1e532fda010074c18d107f80c04ec4102cf3020
Kunal <kunal@kuddle.pet>
2025-06-02 15:54:03 +0530

    #d2p fix env value

--- gh-scrapper.py
+++ gh-scrapper.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 
 # === CONFIGURATION ===
 GITHUB_USERNAME = "kunatastic" 
-GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
+GITHUB_TOKEN = os.environ.get("ACCESSTOKEN")
 
 BASE_URL = "https://api.github.com"
 HEADERS = {
